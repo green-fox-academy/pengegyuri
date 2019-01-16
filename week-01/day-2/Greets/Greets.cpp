6578ae6ec85caed708653d3b03adb3a9c008303e
pengegyuri <pengegyuri@gmail.com>
2019-01-16 20:56:00 +0100

    Greets

---
+++ week-01/day-2/Greets/Greets.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello, Boro!" << std::endl;
+    std::cout << "Hello, Snocc!" << std::endl;
+    std::cout << "Hello, Dani!" << std::endl;
+
+    return 0;
+}
