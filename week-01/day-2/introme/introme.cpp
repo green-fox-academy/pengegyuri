be511efc2637230987996c72859e0dbf07e1da2f
pengegyuri <pengegyuri@gmail.com>
2019-01-16 20:56:58 +0100

    introMe

---
+++ week-01/day-2/introme/introme.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+    std::cout << "My name : Gyuri" << std::endl;
+    std::cout << "My age : 38" << std::endl;
+    std::cout << "My height : 1.75m" << std::endl;
+
+    return 0;
+}
