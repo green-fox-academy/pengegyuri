f61a7d79b2bb1824c6c5c4a3660850b897d447a6
pengegyuri <pengegyuri@gmail.com>
2019-01-17 16:01:33 +0100

    factorial

--- week-01/day-3/factorial/main.cpp
+++ week-01/day-3/factorial/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void factorio(int x, int& y) {
+    y = 1;
+    for (int i = 1; i <= x; i++) {
+        y *= i;
+    }
+
+}
+
+int main(int argc, char* args[]) {
+
+    // - Create a function called `factorio`
+    //   it should calculate its input's factorial.
+    //   it should not return it, but take an additional integer parameter and overwrite its value.
+    int a = 3;
+    int y = 99;
+    factorio(a, y);
+    std::cout << y;
     return 0;
 }
