25d0928aceddaaae9b1023af0b419328479b089f
pengegyuri <pengegyuri@gmail.com>
2019-01-17 15:07:53 +0100

    reverselist

---
+++ week-01/day-3/reverselist/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+    int aj[5] = {3, 4, 5, 6, 7};
+    int t[5];
+    int j = 5;
+    for (int i = 0; i < 5; i++ ) {
+        j = j - 1;
+        t[i] = aj[j];
+        std::cout << t[i] << " ";
+    }
+    
+    return 0;
+}
