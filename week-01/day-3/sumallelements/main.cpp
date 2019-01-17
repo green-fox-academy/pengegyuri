08fbb45fb2c6c2562f11bc0c76068f0d29616d62
pengegyuri <pengegyuri@gmail.com>
2019-01-17 14:33:52 +0100

    sum all elements

---
+++ week-01/day-3/sumallelements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+    int ai[] = {3, 4, 5, 6, 7};
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum = sum + ai[i];
+    }
+    std::cout << sum;
+    return 0;
+}
