53b59eed622e0b7efc82c84c1bc757fa4267295f
pengegyuri <pengegyuri@gmail.com>
2019-02-24 19:35:15 +0100

    pi peri added

---
+++ week-06/day-1/pi/pihead.h
@@ -0,0 +1,10 @@
+//
+// Created by gyuri on 2019.02.24..
+//
+
+#ifndef UNTITLED_PIHEAD_H
+#define UNTITLED_PIHEAD_H
+#define PI 3.14
+#define CIRCLEAREA(rad) (rad * rad * PI)
+#define CIRCLEPERI(rad) (2 * rad * PI)
+#endif //UNTITLED_PIHEAD_H
