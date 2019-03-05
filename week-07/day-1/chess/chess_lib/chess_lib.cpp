61c34925b24a114ba81282873bb91eb2d4fc938a
pengegyuri <pengegyuri@gmail.com>
2019-03-05 14:32:20 +0100

    2 external leds

---
+++ week-07/day-1/chess/chess_lib/chess_lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gyuri on 2019.03.04..
+//
+
+#include "chess_lib.h"
---
+++ week-07/day-1/chess/chess_lib/chess_lib.h
@@ -0,0 +1,15 @@
+//
+// Created by gyuri on 2019.03.04..
+//
+
+#ifndef CHESS_CHESS_LIB_H
+#define CHESS_CHESS_LIB_H
+
+
+class chess_lib
+{
+
+};
+
+
+#endif //CHESS_CHESS_LIB_H
