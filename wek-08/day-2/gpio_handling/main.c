3034bb27f95398e0c30b28c86488edd82d35828d
pengegyuri <pengegyuri@gmail.com>
2019-03-12 23:39:16 +0100

    gpiohand

--- wek-08/day-2/gpio_handling/main.c
+++ wek-08/day-2/gpio_handling/main.c
@@ -16,22 +16,18 @@
 
 
 volatile uint32_t tme;
-volatile int counter = 0;
-volatile uint32_t ext_led;
+volatile int counter = 1;
 int main(void)
 {
     HAL_Init();
     init_gpiof_10_for_L();
     init_gpioa_8_for_B();
-
-    ext_led = (1 << 10);
     tme = HAL_GetTick();
     while (1) {
-    	if (counter % 10 == 0){
-				GPIOF->ODR |= ext_led;
-			}else if (counter % 10 == 5){
-				GPIOF->ODR -= ext_led;
-			}
+    	if (counter % 6 == 0){
+    		counter = 1;
+			GPIOF->ODR ^= (1 << 10);
+		}
     }
 }
 
@@ -44,7 +40,7 @@ void EXTI9_5_IRQHandler(void)
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	if (HAL_GetTick() - tme > 100){
+	if (HAL_GetTick() - tme > 200){
 		counter++;
 		tme = HAL_GetTick();
 	}
