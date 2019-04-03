1d3f3b6500e3cf2da14061b624f3b2ba54537fdc
pengegyuri <pengegyuri@gmail.com>
2019-04-03 16:19:45 +0200

    feat(circle)

---
+++ week-10/circle/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+
+int main(int argc, char** argv )
+{
+	cv::Mat image;
+	image = cv::imread( "../mau.jpg", 1 );
+
+	if ( !image.data )
+	{
+		printf("No image data \n");
+		return -1;
+	}
+
+	cv::namedWindow("Display Image", cv::WINDOW_NORMAL);
+	cv::circle(image, cv::Point(image.cols / 2, image.rows / 2), 50, cv::Scalar(0, 0, 150), 2);
+	cv::imshow("Display Image", image);
+	cv::waitKey(0);
+
+	return 0;
+}
